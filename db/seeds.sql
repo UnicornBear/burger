5d12f8f4b3b858e41e70436a909adaf43b30d747
UnicornBear <jtbregenzer@gmail.com>
2019-06-20 08:56:54 -0600

    updated seed file to include 4 yummy burger options

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- need to enter in the type of burgers one can 'Devour'
+
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Buffaloe Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Fire Cracker');
